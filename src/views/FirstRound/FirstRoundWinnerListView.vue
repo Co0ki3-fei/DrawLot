a977459e450c91998b0c096faf7b4af5e9eb8f8e
ameuu <2421894564@qq.com>
2024-12-04 17:07:38 +0800

    update FistRoundWinnerListView

--- src/views/FirstRound/FirstRoundWinnerListView.vue
+++ src/views/FirstRound/FirstRoundWinnerListView.vue
@@ -1,9 +1,60 @@
-<script setup>
-import TheWelcome from '../../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-      第一轮
-  </main>
+<div class="winner-list-view">
+  <div class="title">
+    三十二强赛胜者
+  </div>
+  <div class="winner-list">
+    <el-table :data="winnerList" border :show-header="false" >
+        <el-table-column prop="winner" align="center">
+            <template #default="{row}">
+                <span>
+                    <span :style="{color:'blue',fontWeight: 'bold',textDecoration:'underline',fontStyle:'italic',fontSize:'18px'}">{{ row.winnerPart1 }}</span>
+                    <span :style="{color:'purple',fontWeight: 'bold',fontSize:'18px',paddingLeft:'5%'}">{{ row.winnerPart2 }}</span>
+                </span>
+            </template>
+        </el-table-column>
+    </el-table>
+  </div>
+  <div class="next-round-bt">
+    <el-button>
+        进入下一轮
+    </el-button>
+  </div>
+</div>
 </template>
+
+<style>
+.winner-list-view {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 5%;
+}
+
+.title {
+    font-size: 32px;
+    font-style: italic;
+    text-decoration: underline;
+    font-weight: bold;
+    padding-bottom: 5%;
+}
+
+.winner-list {
+    width: 30%;
+    padding-bottom: 5%;
+}
+</style>
+
+<script setup>
+
+
+const winnerList = [
+    {"winnerPart1": "A","winnerPart2":"选手1"},
+    {"winnerPart1": "B","winnerPart2":"选手18"},
+    {"winnerPart1": "C","winnerPart2":"选手3"},
+    {"winnerPart1": "D","winnerPart2":"选手19"},
+    {"winnerPart1": "E","winnerPart2":"选手5"},
+    {"winnerPart1": "……","winnerPart2":""}
+]
+
+</script>
